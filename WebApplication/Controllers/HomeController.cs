173e79a6e4c6b4701261f90ecabd8fb4ee2342ff
Vladimir <vladimirrudt@yandex.ru>
2020-03-24 17:11:27 +0300

    Сделано Web приложение(только серверная часть)(Теперь точно)

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication.services;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult GetDataTransferProcessResult([FromServices] IDataTransferProcess dataTransferProcess)
+        {
+            var min = double.Parse(Request.Query["min"]);
+            var max = double.Parse(Request.Query["max"]);
+            var count = int.Parse(Request.Query["count"]);
+            var a = double.Parse(Request.Query["a"]);
+            var b = double.Parse(Request.Query["b"]);
+            var c = double.Parse(Request.Query["c"]);
+            var significanceLevel = double.Parse(Request.Query["sl"]);
+            var functionType = Request.Query["ft"];
+
+            var result = dataTransferProcess.DataTransferProcess(min, max, count, a, b, c, 
+                functionType, significanceLevel);
+
+            return Json(result);
+        }
+        
+    }
+}
